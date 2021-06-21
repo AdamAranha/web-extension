876bfd448344d1992b37a496c00fad52c78dcad6
Adam <a3aranha@gmail.com>
2021-06-21 00:22:40 -0400

    working on homepage component, added container and deciding on colours

--- chromeextension/src/App.js
+++ chromeextension/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/Homepage/Homepage.js'
 
 function App() {
+
+  console.log('This is my new tab now')
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <Homepage />
     </div>
   );
 }
---
+++ chromeextension/src/pages/Homepage/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Homepage.css'
+
+function Homepage() {
+
+    return (
+        <div className='homepage-wrapper'>
+            <p>Hello World</p>
+
+            <div className='homepage-container'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Homepage

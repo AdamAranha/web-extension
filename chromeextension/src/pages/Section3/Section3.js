9d81e2737a266c85b089395dfb05a8cc67dcfe6b
Adam <aearanha@hotmail.com>
2021-07-04 20:23:33 -0400

    Added shortcut add button

--- chromeextension/src/pages/Section3/Section3.js
+++ chromeextension/src/pages/Section3/Section3.js
@@ -8,22 +8,35 @@ function Section3() {
     const [modalEditShow, setModalEditShow] = useState(false)
     const [modalOpacity, setModalOpacity] = useState('100%')
 
+    const [shortList, setShortList] = useState([])
+
     useEffect(() => {
 
     }, [])
 
+    function checkShortList() {
+        setShortList(JSON.parse(localStorage.shortList || '[]'))
+
+        shortList.map(() => {
+
+        })
+    }
+
     return (
         <div className='section3-wrapper'>
             <div className='section3-container'>
+                {/* <Shortcut setModalEditShow={setModalEditShow} modalOpacity={modalOpacity} setModalOpacity={setModalOpacity} />
                 <Shortcut setModalEditShow={setModalEditShow} modalOpacity={modalOpacity} setModalOpacity={setModalOpacity} />
                 <Shortcut setModalEditShow={setModalEditShow} modalOpacity={modalOpacity} setModalOpacity={setModalOpacity} />
                 <Shortcut setModalEditShow={setModalEditShow} modalOpacity={modalOpacity} setModalOpacity={setModalOpacity} />
                 <Shortcut setModalEditShow={setModalEditShow} modalOpacity={modalOpacity} setModalOpacity={setModalOpacity} />
-                <Shortcut setModalEditShow={setModalEditShow} modalOpacity={modalOpacity} setModalOpacity={setModalOpacity} />
-                <Shortcut setModalEditShow={setModalEditShow} modalOpacity={modalOpacity} setModalOpacity={setModalOpacity} />
+                <Shortcut setModalEditShow={setModalEditShow} modalOpacity={modalOpacity} setModalOpacity={setModalOpacity} /> */}
 
 
                 <EditModal modalEditShow={modalEditShow} setModalEditShow={setModalEditShow} />
+                <div className='section3-addShortcut'>
+                    <div className='section3-circle' onClick={() => setModalEditShow(true)}>+</div>
+                </div>
             </div>
         </div>
 
